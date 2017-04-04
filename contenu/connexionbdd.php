e1e0eed76b8a16a54ced92086cd148853a0546f0
aberhault <antoine.berhault@mercuryglobalsolutions.fr>
2017-04-04 16:34:23 +0200

    V2.2

--- contenu/connexionbdd.php
+++ contenu/connexionbdd.php
@@ -4,7 +4,7 @@
 
 try
 {
-	$bdd=new PDO('mysql:host=mysql-test1.192.168.99.100.nip.io;dbname=absence','root',"Bt+4ez9h");
+	$bdd=new PDO('mysql:host=mysql.test1.svc;dbname=absence','root',"Bt+4ez9h");
 	$bdd->exec("set character set utf8");
 	
 	$connect=true;
